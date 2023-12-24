cdb58a36524c79d45be17ada9ff71d08b195eb3b
Omar Faruque <omar@nybsys.com>
2023-12-24 17:47:58 +0600

    add some features

---
+++ open5gs/files/shortcut.sh
@@ -0,0 +1,7 @@
+sudo systemctl $1 open5gs-mmed
+sudo systemctl $1 open5gs-sgwcd
+sudo systemctl $1 open5gs-smfd
+sudo systemctl $1 open5gs-sgwud
+sudo systemctl $1 open5gs-upfd
+sudo systemctl $1 open5gs-hssd
+sudo systemctl $1 open5gs-pcrfd
